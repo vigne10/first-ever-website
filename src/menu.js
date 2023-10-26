24df79d81b606cf2b1eb746ec35733364b57dd14
Brandon Vigne <brandonvigne@outlook.com>
2023-10-26 22:44:38 +0200

    Project added

---
+++ src/menu.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.navbar');
+const burger = document.querySelector('.burger');
+function toggleMenu () {  
+  navbar.classList.toggle('show-nav')
+  }
+  burger.addEventListener('click', toggleMenu);   
+
+  
